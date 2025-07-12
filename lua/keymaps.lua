d27edeed2e4a66744d3b49c8eb4260fe266aeea2
gagahsyuja <gghssa1@gmail.com>
2025-07-12 22:28:20 +0700

    Disable lspsaga terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,11 @@
 
 local keymap = vim.api.nvim_set_keymap
+
 local opts = { noremap = true, silent = true }
 
 keymap("n", "<leader>]", ":bnext<CR>", opts)
 keymap("n", "<leader>[", ":bprevious<CR>", opts)
 keymap("n", "<leader><tab>", ":b#<CR>", opts)
 keymap("n", "<leader><backspace>", ":bd<CR>", opts)
+
 keymap("n", "<leader>hp", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
